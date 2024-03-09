b4fb0bf41f384478ed65e908ea45a550db550d10
ElFabiux <fabiargue33@gmail.com>
2024-03-09 14:56:58 -0600

    solution of the images

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,8 @@
-export default function Card({name}) {
+export default function Card({name, imageUrl}) {
   return (
     <div className="col-sm-12 col-md-6 col-xl-4">
       <div className="card">
-        <img src="../images/faucet.jpg" class="card-img-top" alt="faucet" />
+        <img src={imageUrl} class="card-img-top" alt="faucet" />
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
           <p className="card-text">
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import NavegationVar from "./Components/NavegationVar";
 import Header from "./Components/Header";
 import Card from "./Components/Card";
+const exImage = require.context('./images', true);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,8 +13,8 @@ root.render(
     <NavegationVar />
     <section className="container mt-5">
       <div className="row">
-        <Card name="Pago de Impuestos y Tasas:" />
-        <Card name="Licencias y Permisos" />
+        <Card name="Pago de Impuestos y Tasas" imageUrl={exImage('./faucet.jpg')} />
+        <Card name="Licencias y Permisos" imageUrl={exImage('./bulb.jpg')} />
         <Card name="Gestión de Residuos" />
         <Card name="Registro Civil" />
         <Card name="Servicios Sociales" />
@@ -21,12 +22,6 @@ root.render(
         <Card name="Salud Pública" />
         <Card name="Seguridad Pública" />
         <Card name="Infraestructura y Mantenimiento" />
-        <Card name="Transporte Público" />
-        <Card name="Cultura y Recreación" />
-        <Card name="Desarrollo Económico" />
-        <Card name="Medio Ambiente" />
-        <Card name="Servicios de Emergencia" />
-        <Card name="Desarrollo Comunitario" />
       </div>
     </section>
   </React.StrictMode>
