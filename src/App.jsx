f1823d1d8d45620d75c81590d24160c5578d646e
JorgeRojas720s <jitorojas37@gmail.com>
2024-03-11 00:29:28 -0600

    add Form in page Services

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Card from "./Components/Card";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Form from "./Components/Form";
 
 const exImage = require.context("./images", true);
 
@@ -93,6 +94,7 @@ function App() {
                 <Card
                   key={data.title}
                   name={data.title}
+                  description={data.description}
                   imageUrl={data.imageURL}
                   cardInfo={data}
                   go={clickOnGoButton}
@@ -111,6 +113,7 @@ function App() {
                 <img src={infoCard.imageURL} className="card-img-top rounded-pill mt-5" alt={infoCard.title} />
                 <h2 className="mt-5 text-center">{infoCard.title}</h2>
                 <p className="text-center">{infoCard.description}</p>
+                <Form/>
               </div>
             </div>
           </section>
